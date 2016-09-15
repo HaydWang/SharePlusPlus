49df46823432c8f362133eaa827ccc1012f990fb
a22460 <a22460@motorola.com>
2016-09-15 16:27:56 -0500

    top view

--- app/src/main/java/com/droidrise/shareplusplus/TextDrawer.java
+++ app/src/main/java/com/droidrise/shareplusplus/TextDrawer.java
@@ -1,34 +1,59 @@
 package com.droidrise.shareplusplus;
 
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by a22460 on 16/9/14.
  */
 
 public class TextDrawer {
+    protected final static String DEFAULT_TITLE = "From Share++";
+
     protected static Context mContext;
     protected static WindowManager.LayoutParams mLayoutParams;
+    protected static View topView;
 
     public static void showContent(Context context, String content) {
         mContext = context;
         WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
 
-        final LinearLayout view = (LinearLayout) View.inflate(context, R.layout.view_image, null);
-        TextView tx = (TextView) view.findViewById(R.id.text_content);
+        topView = (View) View.inflate(context, R.layout.view_image, null);
+        TextView tx = (TextView) topView.findViewById(R.id.text_content);
         tx.setText(content);
+
+        FloatingActionButton button = (FloatingActionButton) topView.findViewById(R.id.button_exit);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismissTopView();
+            }
+        });
+
+        button = (FloatingActionButton) topView.findViewById(R.id.button_wechat);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareSnap();
+                dismissTopView();
+            }
+        });
 
         int w = WindowManager.LayoutParams.MATCH_PARENT;
         int h = WindowManager.LayoutParams.WRAP_CONTENT;
@@ -38,39 +63,55 @@ public class TextDrawer {
         mLayoutParams =
                 new WindowManager.LayoutParams(w, h, type, flags, PixelFormat.TRANSPARENT);
         mLayoutParams.gravity = Gravity.TOP;
+        wm.addView(topView, mLayoutParams);
 
-        wm.addView(view, mLayoutParams);
-
-        view.post(new Runnable() {
-            @Override
-            public void run() {
-                saveFrameLayout(view);
-            }
-        });
+//        topView.post(new Runnable() {
+//            @Override
+//            public void run() {
+//                saveFrameLayout(topView);
+//            }
+//        });
     }
 
-    protected static void updateFrameLayout(View view) {
-        mLayoutParams.width = view.getWidth();
-        mLayoutParams.height = view.getHeight();
-
-        WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
-        wm.updateViewLayout(view, mLayoutParams);
+    protected static void dismissTopView() {
+        if (topView != null) {
+            WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+            wm.removeView(topView);
+            topView = null;
+        }
     }
 
-    protected static void saveFrameLayout(View view) {
+    protected static void shareSnap() {
+        File file = saveFrameLayout(topView.findViewById(R.id.layout_snap));
+        if (file != null) {
+            Intent intent = new Intent();
+            ComponentName comp = new ComponentName("com.tencent.mm",
+                    "com.tencent.mm.ui.tools.ShareToTimeLineUI");
+            intent.setComponent(comp);
+            intent.setAction(Intent.ACTION_SEND_MULTIPLE);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setType("image/*");
+            ArrayList<Uri> uris = new ArrayList<>();
+            uris.add(Uri.fromFile(file));
+
+            intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
+            mContext.startActivity(intent);
+        }
+    }
+
+    protected static File saveFrameLayout(View view) {
         int width = view.getWidth();
         int height = view.getHeight();
         Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         Canvas canvas = new Canvas(bitmap);
         view.draw(canvas);
 
-        File folder, file;
-        if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED)) {
-            folder = new File(android.os.Environment.getExternalStorageDirectory(), "Share++");
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            File folder = new File(Environment.getExternalStorageDirectory(), "Share++");
             if (!folder.exists()) {
                 folder.mkdirs();
             }
-            file = new File(folder.getAbsolutePath() + folder.separator + "snap" + ".jpg");
+            File file = new File(folder.getAbsolutePath() + folder.separator + "snap" + ".jpg");
 
             try {
                 FileOutputStream ostream = new FileOutputStream(file);
@@ -79,6 +120,18 @@ public class TextDrawer {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+            return file;
         }
+
+        return null;
+    }
+
+    protected static void updateFrameLayout(View view) {
+        mLayoutParams.width = view.getWidth();
+        mLayoutParams.height = view.getHeight();
+
+        WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
+        wm.updateViewLayout(view, mLayoutParams);
     }
 }
